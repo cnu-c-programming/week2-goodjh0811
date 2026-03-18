4ed6443c9f0c0c9aad4fe1b3b4e6b740b3d95062
goodjh0811 <goodjh0811@gmail.com>
2026-03-18 21:34:16 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -4,7 +4,7 @@ int main()
 {
     int i, j;
     for(i =  1; i <= 9; i++) {
-        for(j = i; j <= 9; j++) {
+        for(j = 1; j <= 9; j++) {
             printf("%d * %d = %d\n", i, j, i*j);
         }
     }

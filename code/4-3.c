329f91ce5473ee2354e41d3f786c8f69c5c5553a
goodjh0811 <goodjh0811@gmail.com>
2026-03-18 21:08:28 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,31 @@
 
 int main()
 {
+    int n, i;
+    int isPrime = 1;
+
+    scanf("%d", &n);
+
+    if (n <= 1)
+    {
+        isPrime = 0;
+    }
+    else
+    {
+        for (i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("true\n");
+    else
+        printf("false\n");
 
     return 0;
 }
-
